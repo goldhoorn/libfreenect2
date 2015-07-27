8a64a8700767cb2e0365314e2271face091e29d9
Matthias Goldhoorn <matthias.goldhoorn@uni-bremen.de>
2015-07-27 10:39:52 +0200

    Correct find_scrpipt for libusb
    The DEPENDS is only set for a local installation.
    Otherwise the system (global) one should used.
    Furthermore the check if libusb was actually found
    (even reuqired) was broken

--- examples/protonect/cmake_modules/FindLibUSB.cmake
+++ examples/protonect/cmake_modules/FindLibUSB.cmake
@@ -8,35 +8,37 @@
 #  LibUSB_LIBRARIES:  the libraries
 
 IF(PKG_CONFIG_FOUND)
-  SET(ENV{PKG_CONFIG_PATH} "${DEPENDS_DIR}/libusb/lib/pkgconfig")
+  if(${DEPENDS_DIR}) #Otherwise use System pkg-config path
+    SET(ENV{PKG_CONFIG_PATH} "${DEPENDS_DIR}/libusb/lib/pkgconfig")
+  endif()
   PKG_CHECK_MODULES(LibUSB libusb-1.0)
   RETURN()
-ENDIF()
+else()
+    FIND_PATH(LibUSB_INCLUDE_DIRS
+      NAMES libusb.h
+      PATHS
+        "${DEPENDS_DIR}/libusb"
+        "${DEPENDS_DIR}/libusbx"
+        ENV LibUSB_ROOT
+      PATH_SUFFIXES
+        include
+        libusb
+    )
 
-FIND_PATH(LibUSB_INCLUDE_DIRS
-  NAMES libusb.h
-  PATHS
-    "${DEPENDS_DIR}/libusb"
-    "${DEPENDS_DIR}/libusbx"
-    ENV LibUSB_ROOT
-  PATH_SUFFIXES
-    include
-    libusb
-)
-
-FIND_LIBRARY(LibUSB_LIBRARIES
-  NAMES libusb-1.0
-  PATHS
-    "${DEPENDS_DIR}/libusb"
-    "${DEPENDS_DIR}/libusbx"
-    ENV LibUSB_ROOT
-  PATH_SUFFIXES
-    x64/Release/dll
-    x64/Debug/dll
-    Win32/Release/dll
-    Win32/Debug/dll
-    MS64
-)
+    FIND_LIBRARY(LibUSB_LIBRARIES
+      NAMES libusb-1.0
+      PATHS
+        "${DEPENDS_DIR}/libusb"
+        "${DEPENDS_DIR}/libusbx"
+        ENV LibUSB_ROOT
+      PATH_SUFFIXES
+        x64/Release/dll
+        x64/Debug/dll
+        Win32/Release/dll
+        Win32/Debug/dll
+        MS64
+    )
+endif()
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibUSB DEFAULT_MSG LibUSB_LIBRARIES LibUSB_INCLUDE_DIRS)
